eba3a61ee1476c32e106fae8ef79e87550a3665e
Rajesh Pillai <pillai.rajesh@gmail.com>
2015-04-28 23:22:05 +0530

    8. Add tweening to intro label.

--- game/menu.js
+++ game/menu.js
@@ -2,19 +2,34 @@ var menuState = {
   create: function() {
     // Add a background image
     game.add.image(0, 0, 'background');
-    // Display the name of the game
-    var nameLabel = game.add.text(game.world.centerX, 80, 'Super Coin Box',
-    { font: '50px Arial', fill: '#ffffff' });
+    
+    // Change the y position to -50, so we don't see the label
+    var nameLabel = game.add.text(game.world.centerX, -50, 'Super Coin Box',
+                  { font: '50px Arial', fill: '#ffffff' });
+    
     nameLabel.anchor.setTo(0.5, 0.5);
+    
+    // Create a tween on the label
+    var tween = game.add.tween(nameLabel);
+    
+    // Change the y position of the label to 80, in 1000 ms
+    tween.to({y:80}, 1000);
+    
+    // Start the tween
+    tween.start();
+    
     // Show the score at the center of the screen
-    var scoreLabel = game.add.text(game.world.centerX, game.world.centerY,'score: ' + game.global.score,
-                                   { font: '25px Arial', fill: '#ffffff' });
+    var scoreLabel = game.add.text(game.world.centerX,
+                        game.world.centerY,'score: ' + game.global.score,
+                        { font: '25px Arial', fill: '#ffffff' });
     scoreLabel.anchor.setTo(0.5, 0.5);
     
     // Explain how to start the game
     
-    var startLabel = game.add.text(game.world.centerX, game.world.height-80, 'press the up arrow key to start',
-                                   { font: '25px Arial', fill: '#ffffff' });
+    var startLabel = game.add.text(game.world.centerX, 
+                           game.world.height-80, 
+                           'press the up arrow key to start',
+                           { font: '25px Arial', fill: '#ffffff' });
 
     startLabel.anchor.setTo(0.5, 0.5);
 
@@ -23,6 +38,7 @@ var menuState = {
     // When the 'upKey' is pressed, it will call the 'start' function once
     upKey.onDown.addOnce(this.start, this);
   },
+  
   start: function() {
     // Start the actual game
     game.state.start('play');
