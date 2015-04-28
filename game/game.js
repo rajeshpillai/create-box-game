e9229efe6c578b4637cd39891fd01bfe61c83bbc
Rajesh Pillai <pillai.rajesh@gmail.com>
2015-04-28 23:01:01 +0530

    5. Adding the play state.

--- game/game.js
+++ game/game.js
@@ -10,6 +10,7 @@ game.global = {
 game.state.add('boot', bootState);
 game.state.add('load', loadState);
 game.state.add('menu', menuState);
+game.state.add('play', playState);
 
 // Start the 'boot' state
 game.state.start('boot');
